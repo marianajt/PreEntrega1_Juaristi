6ff065dae87e47379bc4e48c7aa0f57bf5b87e76
Mariana Juaristi <mariana_juaristi@hotmail.com>
2023-07-20 19:44:27 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-
-
-let P1 = parseInt(prompt("Calificacion del primer alumno"))
-let P2 = parseInt(prompt("Calificacion del segundo alumno"))
-let P3 = parseInt(prompt("Calificacion del tercer alumno"))
-let P4 = parseInt(prompt("Calificacion del cuarto alumno"))
-let P5 = parseInt(prompt("Calificacion del quinto alumno"))
-
-
-function Avg(){
-    alert("El promedio de la clase fue " + (A1+A2+A3+A4+A5)/5)
-}
-
-if((Avg > 60)){
-    alert("El promedio fue mayor de 60, aprobaron!");
-}
-else{
-    alert("A los alumnos se les dificulto la materia, su promedio fue menor de 60")
-}
-
+function calcularPromedio() {
+    let totalCalificaciones = 5;
+    let sumaCalificaciones = 0;
+  
+    let contador = 1;
+    while (contador <= totalCalificaciones) {
+      let calificacion = parseFloat(prompt(`Ingresa la calificación ${contador}:`));
+  
+      if (isNaN(calificacion)) {
+        alert("Por favor, ingresa un número válido.");
+        continue;
+      }
+  
+      sumaCalificaciones += calificacion;
+      contador++;
+    }
+  
+    let promedio = sumaCalificaciones / totalCalificaciones;
+  
+    if (promedio >= 60) {
+      alert('El promedio fue mayor de 60, aprobaron!"');
+    } else {
+      alert('A los alumnos se les dificulto la materia, su promedio fue menor de 60');
+    }
+  }
+  
+  calcularPromedio();
+  
